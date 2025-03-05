ba8a793d3e2837ca1580884d9d0a3bfe5e158558
Tariq Areesh <109058265+tariq-areesh@users.noreply.github.com>
2025-03-05 23:36:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     async function fetchData() {
         try {
-            let response = await fetch("http://192.168.8.194/data"); 
+            let response = await fetch("http://(esp_ip)/data"); 
             let json = await response.json();
             document.getElementById("value").textContent = json.value;
         } catch (error) {
